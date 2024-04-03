648e797ff14f8659c80021ffe850904d6a31e9d2
georgiasantos-frontend <georgiasantos.frontend@gmail.com>
2024-04-03 16:25:18 +0100

    first-project-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+//JSX -> MISTURA JS COM HTML
 function App() {
+  const hello = "Hello React";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //o return serve para retornar só o html
+    <div>
+      <h1>{hello}</h1>
     </div>
   );
-}
+} //consigo colocar js dentro do html, basta colocar {}
 
 export default App;
